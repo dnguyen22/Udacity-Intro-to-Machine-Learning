39c9e0d54f1b9399a9e4ec888b8606f6145a41d5
dnguyen22 <don.nguyen22@gmail.com>
2017-12-05 15:15:43 -0600

    Add classifier

--- Projects/svm/svm_author_id.py
+++ Projects/svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +21,14 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
+clf = SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time: ", round(time() - t0, 3), "s")
+pred = clf.predict(features_test)
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+accuracy = accuracy_score(pred, labels_test)
+print("Accuracy Score: {}".format(accuracy))
