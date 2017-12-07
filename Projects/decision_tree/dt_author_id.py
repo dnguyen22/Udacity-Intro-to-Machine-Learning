aed02e70f95a9af13ad3c2f40aef703121a9aa4f
dnguyen22 <don.nguyen22@gmail.com>
2017-12-06 21:15:09 -0600

    Add decision tree classifier

--- Projects/decision_tree/dt_author_id.py
+++ Projects/decision_tree/dt_author_id.py
@@ -12,20 +12,19 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time: ", round(time() - t0, 3), "s")
+pred = clf.predict(features_test)
 
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+accuracy = accuracy_score(pred, labels_test)
+print("Accuracy Score: {}".format(accuracy))
